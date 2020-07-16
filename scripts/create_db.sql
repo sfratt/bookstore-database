b8575b3f1a47476cf4a29d8250473de292713374
sfratt <sfratt@users.noreply.github.com>
2020-07-16 17:52:59 -0400

    Remove queries from database creation script

--- scripts/create_db.sql
+++ scripts/create_db.sql
@@ -2,6 +2,8 @@ CREATE DATABASE IF NOT EXISTS bookstore;
 USE bookstore;
 
 DROP TABLE IF EXISTS Customers;
+DROP TABLE IF EXISTS Orders;
+
 
 CREATE TABLE Customers (
 	CustomerId INT,
@@ -15,17 +17,12 @@ CREATE TABLE Customers (
     Province VARCHAR(2),
     CompanyName VARCHAR(100),
     CumulativePurchases INT,
-    primary key (CustomerId)
+    PRIMARY KEY (CustomerId)
 );
 
 INSERT INTO Customers
 VALUE (1, "John", "Doe", "john@doe.com", "123-456-6789", "123 John St", "1T2Y3U", "Montreal", "QC", "JohnDoe", 4);
 
-SELECT *
-FROM Customers;
-
-
-DROP TABLE IF EXISTS Orders;
 
 CREATE TABLE Orders (
 	OrderId INT,
@@ -35,11 +32,8 @@ CREATE TABLE Orders (
     QuantityOrdered INT,
     BranchId INT,
     PublisherId INT,
-    primary key (OrderId)
+    PRIMARY KEY (OrderId)
 );
 
 INSERT INTO Orders
 VALUE (1, " 0-684-8328-5", "00084848554", "2020-7-04", "2", "1", "1");
-
-SELECT *
-FROM Orders;
