f706d168c08ff95df1a2f553c3771a6e8bf6f0ac
sfratt <sfratt@users.noreply.github.com>
2020-07-20 14:46:33 -0400

    Clean up create database script

--- database/create_db.sql
+++ database/create_db.sql
@@ -2,14 +2,16 @@ DROP DATABASE IF EXISTS Bookstore;
 CREATE DATABASE Bookstore;
 USE Bookstore;
 
-CREATE TABLE Bookstores (
-    BookstoreId INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE Bookstores
+(
+    BookstoreId INT NOT NULL AUTO_INCREMENT,
     Address VARCHAR(100),
     PRIMARY KEY (BookstoreId)
-)  AUTO_INCREMENT=1;
+);
 
-CREATE TABLE Customers (
-    CustomerId INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Customers
+(
+    CustomerId INT NOT NULL AUTO_INCREMENT,
     FirstName VARCHAR(100),
     LastName VARCHAR(100),
     Email VARCHAR(100),
@@ -18,26 +20,29 @@ CREATE TABLE Customers (
     PostalCode VARCHAR(6),
     City VARCHAR(100),
     Province VARCHAR(2),
-    CompanyName VARCHAR(100)
-)  AUTO_INCREMENT=1;
-
-CREATE TABLE Authors (
-    AuthorId INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    Name VARCHAR(100)
-)  AUTO_INCREMENT=1;
-
-CREATE TABLE ReaderInterests (
-    AuthorId INT UNSIGNED NOT NULL,
-    CustomerId INT UNSIGNED NOT NULL,
-    FOREIGN KEY (AuthorId)
-        REFERENCES Authors (AuthorId),
-    FOREIGN KEY (CustomerId)
-        REFERENCES Customers (CustomerId),
-    PRIMARY KEY (AuthorId , CustomerId)
+    CompanyName VARCHAR(100),
+    PRIMARY KEY (CustomerId)
 );
 
-CREATE TABLE Publishers (
-    PublisherId INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Authors
+(
+    AuthorId INT NOT NULL AUTO_INCREMENT,
+    Name VARCHAR(100),
+    PRIMARY KEY (AuthorId)
+);
+
+CREATE TABLE ReaderInterests
+(
+    AuthorId INT NOT NULL,
+    CustomerId INT NOT NULL,
+    PRIMARY KEY (AuthorId, CustomerId),
+    FOREIGN KEY (AuthorId) REFERENCES Authors (AuthorId),
+    FOREIGN KEY (CustomerId) REFERENCES Customers (CustomerId)
+);
+
+CREATE TABLE Publishers
+(
+    PublisherId INT NOT NULL AUTO_INCREMENT,
     Website VARCHAR(100),
     Email VARCHAR(100),
     Phone VARCHAR(12),
@@ -45,11 +50,13 @@ CREATE TABLE Publishers (
     Address VARCHAR(100),
     City VARCHAR(100),
     Province VARCHAR(2),
-    PostalCode VARCHAR(6)
-)  AUTO_INCREMENT=1;
+    PostalCode VARCHAR(6),
+    PRIMARY KEY (PublisherId)
+);
 
-CREATE TABLE Branches (
-    BranchId INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Branches
+(
+    BranchId INT NOT NULL AUTO_INCREMENT,
     BranchName VARCHAR(100),
     Representative VARCHAR(100),
     Email VARCHAR(100),
@@ -58,69 +65,68 @@ CREATE TABLE Branches (
     City VARCHAR(100),
     Province VARCHAR(2),
     PostalCode VARCHAR(6),
-    PublisherId INT UNSIGNED NOT NULL,
-    FOREIGN KEY (PublisherId)
-        REFERENCES Publishers (PublisherId)
-)  AUTO_INCREMENT=1;
+    PublisherId INT NOT NULL,
+    PRIMARY KEY (BranchId),
+    FOREIGN KEY (PublisherId) REFERENCES Publishers (PublisherId)
+);
 
-CREATE TABLE Books (
-    ISBN VARCHAR(13) NOT NULL PRIMARY KEY,
+CREATE TABLE Books
+(
+    ISBN VARCHAR(13) NOT NULL,
     Title VARCHAR(200),
     BookSubject VARCHAR(100),
     QuantityOnHand INT,
     YearToDateQuantitySold INT,
-    CostPrice DECIMAL(6 , 2 ),
-    SellingPrice DECIMAL(6 , 2 ),
-    AuthorId INT UNSIGNED NOT NULL,
-    PublisherId INT UNSIGNED NOT NULL,
-    FOREIGN KEY (AuthorId)
-        REFERENCES Authors (AuthorId),
-    FOREIGN KEY (PublisherId)
-        REFERENCES Publishers (PublisherId)
+    CostPrice DECIMAL(6, 2),
+    SellingPrice DECIMAL(6, 2),
+    AuthorId INT NOT NULL,
+    PublisherId INT NOT NULL,
+    PRIMARY KEY (ISBN),
+    FOREIGN KEY (AuthorId) REFERENCES Authors (AuthorId),
+    FOREIGN KEY (PublisherId) REFERENCES Publishers (PublisherId)
 );
 
-CREATE TABLE Inventories (
-    BookstoreId INT UNSIGNED NOT NULL,
+CREATE TABLE Inventories
+(
+    BookstoreId INT NOT NULL AUTO_INCREMENT,
     QuantityOnHand INT,
     ISBN VARCHAR(13) NOT NULL,
-    PRIMARY KEY (BookstoreId , ISBN),
-    FOREIGN KEY (BookstoreId)
-        REFERENCES Bookstores (BookstoreId),
-    FOREIGN KEY (ISBN)
-        REFERENCES Books (ISBN)
+    PRIMARY KEY (BookstoreId, ISBN),
+    FOREIGN KEY (BookstoreId) REFERENCES Bookstores (BookstoreId),
+    FOREIGN KEY (ISBN) REFERENCES Books (ISBN)
 );
 
-CREATE TABLE BranchHasBooks (
-    BranchId INT UNSIGNED NOT NULL,
+CREATE TABLE BranchHasBooks
+(
+    BranchId INT NOT NULL,
     ISBN VARCHAR(13) NOT NULL,
     Quantity INT,
-    PRIMARY KEY (BranchId , ISBN)
+    PRIMARY KEY (BranchId, ISBN)
 );
 
-CREATE TABLE Transactions (
-    TransactionId INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    Price DECIMAL(4 , 2 ),
+CREATE TABLE Transactions
+(
+    TransactionId INT NOT NULL,
+    Price DECIMAL(4, 2),
     Quantity INT,
     ISBN VARCHAR(13) NOT NULL,
-    CustomerId INT UNSIGNED NOT NULL,
-    BookstoreId INT UNSIGNED NOT NULL,
-    FOREIGN KEY (CustomerId)
-        REFERENCES Customers (CustomerId),
-    FOREIGN KEY (BookstoreId)
-        REFERENCES Bookstores (BookstoreId)
-)  AUTO_INCREMENT=1;
+    CustomerId INT NOT NULL,
+    BookstoreId INT NOT NULL,
+    PRIMARY KEY (TransactionId),
+    FOREIGN KEY (CustomerId) REFERENCES Customers (CustomerId),
+    FOREIGN KEY (BookstoreId) REFERENCES Bookstores (BookstoreId)
+);
 
-CREATE TABLE Orders (
-    OrderNumber INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Orders
+(
+    OrderNumber INT NOT NULL AUTO_INCREMENT,
     OrderDate DATE,
     QuantityOrdered INT,
     ISBN VARCHAR(13),
-    PublisherId INT UNSIGNED NOT NULL,
-    BranchId INT UNSIGNED NOT NULL,
-    FOREIGN KEY (ISBN)
-        REFERENCES Books (ISBN),
-    FOREIGN KEY (PublisherId)
-        REFERENCES Publishers (PublisherId),
-    FOREIGN KEY (BranchId)
-        REFERENCES Branches (BranchId)
-)  AUTO_INCREMENT=1;
+    PublisherId INT NOT NULL,
+    BranchId INT NOT NULL,
+    PRIMARY KEY (OrderNumber),
+    FOREIGN KEY (ISBN) REFERENCES Books (ISBN),
+    FOREIGN KEY (PublisherId) REFERENCES Publishers (PublisherId),
+    FOREIGN KEY (BranchId) REFERENCES Branches (BranchId)
+);
--- database/seed_db.sql
+++ database/seed_db.sql
@@ -13,71 +13,71 @@ VALUES
 
 INSERT INTO Customers --(id,firstname,lastname,email,phone,address,postalcode,city,province,companyName) 
 VALUES
-    (1,"Carlos","Mayer","sed.dui@ipsum.co.uk","4489980672","Ap #701-5466 Donec Av.","G1L0P5","Richmond","QC","Mauris Nulla Corp."),
-    (2,"Sacha","Fleming","Nulla@felispurusac.ca","3284912130","2892 Risus. Avenue","N3M0H2","Essex","ON","Volutpat PC"),
-    (3,"Eliana","Holt","lorem.ac@semmollis.net","9333638412","606-7765 Posuere Street","J6C1R5","Saint-Hilarion","QC","Ut Tincidunt Vehicula Institute"),
-    (4,"Tana","Potter","lectus.Nullam@lobortisClassaptent.net","4162164060","P.O. Box 867, 6193 At Rd.","T9A0V2","Calmar","AB","Ligula Tortor PC"),
-    (5,"Galvin","Middleton","magna.tellus.faucibus@neque.org","5398889875","385-508 Tempus, Road","V3W3V1","Cariboo Regional District","BC","Gravida LLC"),
-    (6,"Laith","Snider","at.pede.Cras@euismod.com","4111785748","6649 Et Ave","T7A0J4","Trochu","AB","Id Sapien Company"),
-    (7,"Donna","Raymond","Nullam.suscipit.est@anteiaculisnec.edu","2719483807","173-3595 Adipiscing Rd.","T6X0Y7","Edmonton","AB","Urna Ut Associates"),
-    (8,"Camille","Mckenzie","sed.tortor.Integer@magnamalesuadavel.co.uk","6829254716","2895 Enim Road","X7B7Y4","Iqaluit","NU","Tellus Ltd"),
-    (9,"Len","Walker","Suspendisse@eratvolutpatNulla.edu","7013684404","572-5614 Phasellus Rd.","Y7M1N3","Whitehorse","YT","Blandit Limited"),
-    (10,"Simone","Leon","non.quam.Pellentesque@loremsemperauctor.co.uk","4626947612","859-7708 Placerat, Street","V8J5X6","Terrance","BC","Aenean Eget Inc.");
+    (1, "Carlos", "Mayer", "sed.dui@ipsum.co.uk", "4489980672", "Ap #701-5466 Donec Av.", "G1L0P5", "Richmond", "QC", "Mauris Nulla Corp."),
+    (2, "Sacha", "Fleming", "Nulla@felispurusac.ca", "3284912130", "2892 Risus. Avenue", "N3M0H2", "Essex", "ON", "Volutpat PC"),
+    (3, "Eliana", "Holt", "lorem.ac@semmollis.net", "9333638412", "606-7765 Posuere Street", "J6C1R5", "Saint-Hilarion", "QC", "Ut Tincidunt Vehicula Institute"),
+    (4, "Tana", "Potter", "lectus.Nullam@lobortisClassaptent.net", "4162164060", "P.O. Box 867, 6193 At Rd.", "T9A0V2", "Calmar", "AB", "Ligula Tortor PC"),
+    (5, "Galvin", "Middleton", "magna.tellus.faucibus@neque.org", "5398889875", "385-508 Tempus, Road", "V3W3V1", "Cariboo Regional District", "BC", "Gravida LLC"),
+    (6, "Laith", "Snider", "at.pede.Cras@euismod.com", "4111785748", "6649 Et Ave", "T7A0J4", "Trochu", "AB", "Id Sapien Company"),
+    (7, "Donna", "Raymond", "Nullam.suscipit.est@anteiaculisnec.edu", "2719483807", "173-3595 Adipiscing Rd.", "T6X0Y7", "Edmonton", "AB", "Urna Ut Associates"),
+    (8, "Camille", "Mckenzie", "sed.tortor.Integer@magnamalesuadavel.co.uk", "6829254716", "2895 Enim Road", "X7B7Y4", "Iqaluit", "NU", "Tellus Ltd"),
+    (9, "Len", "Walker", "Suspendisse@eratvolutpatNulla.edu", "7013684404", "572-5614 Phasellus Rd.", "Y7M1N3", "Whitehorse", "YT", "Blandit Limited"),
+    (10, "Simone", "Leon", "non.quam.Pellentesque@loremsemperauctor.co.uk", "4626947612", "859-7708 Placerat, Street", "V8J5X6", "Terrance", "BC", "Aenean Eget Inc.");
 
 
-INSERT INTO Authors-- (id,name)
+INSERT INTO Authors -- (id,name)
 VALUES
-    (1,"Lois Walls"),
-    (2,"Phillip Bass"),
-    (3,"Bradley Woodard"),
-    (4,"William Kim"),
-    (5,"Wynter House"),
-    (6,"Steel Wyatt"),
-    (7,"Dane Henderson"),
-    (8,"Keane Ramirez"),
-    (9,"Shellie Little"),
-    (10,"Maile Crane");
+    (1, "Lois Walls"),
+    (2, "Phillip Bass"),
+    (3, "Bradley Woodard"),
+    (4, "William Kim"),
+    (5, "Wynter House"),
+    (6, "Steel Wyatt"),
+    (7, "Dane Henderson"),
+    (8, "Keane Ramirez"),
+    (9, "Shellie Little"),
+    (10, "Maile Crane");
 
 --TODO ReaderInterests
 
 INSERT INTO Publishers --(id, website, email, phone, companyName, address, city, province, postalcode)
 VALUES
-    (1,        "publisherweb.info","sed.sapien.Nunc@diam.net","8933717051","Convallis Ligula Donec Consulting","124-9479 Lectus Ave","Chesapeake","VA","V3N4B6"),
-    (2,                  "publishernow.org","quis.tristique.ac@volutpatNulla.co.uk","3471083820","Elit A Feugiat LLC","291-6511 Ipsum. Ave","Frederick","MD","L1N3M1"),
-    (3,                    "publisher-me.com","dictum.mi@velnislQuisque.com","7333852178","Feugiat Inc.","340-9985 Nec Av.","Stamford","CT","Q2N3N2"),
-    (4,                "publisherhome.info","mi.eleifend@nonluctussit.edu","3702237251","Malesuada Fringilla Est Industries","914-9798 Lacinia Ave","Shawinigan","QC","G6B1A0"),
-    (5,    "mypublisher.blog","et.rutrum.non@sociisnatoque.com","3628282066","Placerat Orci Lacus Industries","3640 Pulvinar St.","New Orleans","LA","N3M2V1"),
-    (6,       "publisher-online.net","vitae@velesttempor.org","2649082829","Tempor Arcu Vestibulum Foundation","224-7883 Quisque Avenue","Newport News","VA","B9S1K2"),
-    (7,            "publisherstar.org","ultrices.posuere@vulputateeuodio.ca","7609143376","Duis A Mi Inc.","P.O. Box 504, 5906 Consequat Avenue","Green Bay","WI","V2Z3E0"),
-    (8,    "publisher-me.blog","molestie.Sed.id@feugiatnecdiam.net","9081194200","Orci PC","680-4469 Consectetuer Road","Mission","BC","V6N6B6"),
-    (9,            "publisherme.info","vestibulum@accumsanconvallis.net","7245284833","Augue Scelerisque Mollis Industries","P.O. Box 250, 5221 Tempor Rd.","Portland","ME","H4NB5Z"),
-    (10,                   "yourpublisher.blog","mus.Proin.vel@dolorNulla.edu","2099826339","Vitae LLP","685 Eros St.","Carleton","QC","H6Z9M6");
+    (1, "publisherweb.info", "sed.sapien.Nunc@diam.net", "8933717051", "Convallis Ligula Donec Consulting", "124-9479 Lectus Ave", "Chesapeake", "VA", "V3N4B6"),
+    (2, "publishernow.org", "quis.tristique.ac@volutpatNulla.co.uk", "3471083820", "Elit A Feugiat LLC", "291-6511 Ipsum. Ave", "Frederick", "MD", "L1N3M1"),
+    (3, "publisher-me.com", "dictum.mi@velnislQuisque.com", "7333852178", "Feugiat Inc.", "340-9985 Nec Av.", "Stamford", "CT", "Q2N3N2"),
+    (4, "publisherhome.info", "mi.eleifend@nonluctussit.edu", "3702237251", "Malesuada Fringilla Est Industries", "914-9798 Lacinia Ave", "Shawinigan", "QC", "G6B1A0"),
+    (5, "mypublisher.blog", "et.rutrum.non@sociisnatoque.com", "3628282066", "Placerat Orci Lacus Industries", "3640 Pulvinar St.", "New Orleans", "LA", "N3M2V1"),
+    (6, "publisher-online.net", "vitae@velesttempor.org", "2649082829", "Tempor Arcu Vestibulum Foundation", "224-7883 Quisque Avenue", "Newport News", "VA", "B9S1K2"),
+    (7, "publisherstar.org", "ultrices.posuere@vulputateeuodio.ca", "7609143376", "Duis A Mi Inc.", "P.O. Box 504, 5906 Consequat Avenue", "Green Bay", "WI", "V2Z3E0"),
+    (8, "publisher-me.blog", "molestie.Sed.id@feugiatnecdiam.net", "9081194200", "Orci PC", "680-4469 Consectetuer Road", "Mission", "BC", "V6N6B6"),
+    (9, "publisherme.info", "vestibulum@accumsanconvallis.net", "7245284833", "Augue Scelerisque Mollis Industries", "P.O. Box 250, 5221 Tempor Rd.", "Portland", "ME", "H4NB5Z"),
+    (10, "yourpublisher.blog", "mus.Proin.vel@dolorNulla.edu", "2099826339", "Vitae LLP", "685 Eros St.", "Carleton", "QC", "H6Z9M6");
 
 INSERT INTO Branches --(id,branchName,representative,email,phone,address,city,province,postalcode,publisherid)
-VALUES 
-    (1,"Et Malesuada Fames PC","Ina Atkinson","Mauris.vel.turpis@leoCrasvehicula.co.uk","9807492551","8608 Vitae Rd.","Olathe","KS","P9N1M6",1),
-    (2,"Cursus Corp.","Blair Vance","et.ultrices.posuere@semper.edu","8142692845","800-3636 Sem. Street","Paradise","NL","A6R2R0",2),
-    (3,"Odio Nam Limited","Pandora Abbott","accumsan@Proinvelit.co.uk","8682575094","521 At, Street","Bangor","ME","W3A1N3",3),
-    (4,"Libero Ltd","Yoshio Coffey","Integer.id@Suspendisse.co.uk","4882559133","P.O. Box 709, 4789 Luctus Rd.","Oklahoma City","OK","H2N3V1",4),
-    (5,"Nunc Institute","Ivana Burks","sodales@vehiculaaliquetlibero.co.uk","4236053299","574-250 Morbi St.","Tampa","FL","A3B3V9",5),
-    (6,"Tortor Associates","Demetria Marquez","Donec.elementum@natoquepenatibus.co.uk","1334893192","5695 Consectetuer Avenue","Lethbridge","AB","T9H3X8",6),
-    (7,"Aliquam Eu Accumsan LLC","Michelle Tran","nonummy.ac.feugiat@etcommodo.net","4314603971","P.O. Box 355, 5004 Elit Avenue","Tulsa","OK","S9S8J1",7),
-    (8,"Lorem Luctus Company","Ainsley Key","at.iaculis@nec.co.uk","1327000832","P.O. Box 825, 7375 Et St.","Anchorage","AK","Z7C7E8",8),
-    (9,"Sit Amet Orci LLP","Caesar Ray","urna.Ut.tincidunt@euligula.org","7622075599","P.O. Box 968, 3823 Velit Rd.","Vilna","AB","T4E0W3",9),
-    (10,"Ante Maecenas Corp.","Hayfa Glass","gravida.molestie.arcu@lobortisauguescelerisque.net","7624375646","855-2020 Curabitur Road","Flint","MI","L2B4N1",10);
+VALUES
+    (1, "Et Malesuada Fames PC", "Ina Atkinson", "Mauris.vel.turpis@leoCrasvehicula.co.uk", "9807492551", "8608 Vitae Rd.", "Olathe", "KS", "P9N1M6", 1),
+    (2, "Cursus Corp.", "Blair Vance", "et.ultrices.posuere@semper.edu", "8142692845", "800-3636 Sem. Street", "Paradise", "NL", "A6R2R0", 2),
+    (3, "Odio Nam Limited", "Pandora Abbott", "accumsan@Proinvelit.co.uk", "8682575094", "521 At, Street", "Bangor", "ME", "W3A1N3", 3),
+    (4, "Libero Ltd", "Yoshio Coffey", "Integer.id@Suspendisse.co.uk", "4882559133", "P.O. Box 709, 4789 Luctus Rd.", "Oklahoma City", "OK", "H2N3V1", 4),
+    (5, "Nunc Institute", "Ivana Burks", "sodales@vehiculaaliquetlibero.co.uk", "4236053299", "574-250 Morbi St.", "Tampa", "FL", "A3B3V9", 5),
+    (6, "Tortor Associates", "Demetria Marquez", "Donec.elementum@natoquepenatibus.co.uk", "1334893192", "5695 Consectetuer Avenue", "Lethbridge", "AB", "T9H3X8", 6),
+    (7, "Aliquam Eu Accumsan LLC", "Michelle Tran", "nonummy.ac.feugiat@etcommodo.net", "4314603971", "P.O. Box 355, 5004 Elit Avenue", "Tulsa", "OK", "S9S8J1", 7),
+    (8, "Lorem Luctus Company", "Ainsley Key", "at.iaculis@nec.co.uk", "1327000832", "P.O. Box 825, 7375 Et St.", "Anchorage", "AK", "Z7C7E8", 8),
+    (9, "Sit Amet Orci LLP", "Caesar Ray", "urna.Ut.tincidunt@euligula.org", "7622075599", "P.O. Box 968, 3823 Velit Rd.", "Vilna", "AB", "T4E0W3", 9),
+    (10, "Ante Maecenas Corp.", "Hayfa Glass", "gravida.molestie.arcu@lobortisauguescelerisque.net", "7624375646", "855-2020 Curabitur Road", "Flint", "MI", "L2B4N1", 10);
 
 INSERT INTO Books --( ISBN, Title , BookSubject, QuantityOnHand, YearToDateQuantitySold, CostPrice, SellingPrice, AuthorId, PublisherId)
 VALUES
-    ("9781783085569", "A Midsummer Night's Dream in Context: Magic, Madness and Mayhem", "English Literature",40 , 336 , 22.5, 27.99,1,1 ),
-    ("9780748638482", "The Alids: The First Family of Islam", "Religion",21  , 613, 20, 24.99,2,2 ), 
-    ("9780511555121", "And so we Came to Rome: The Political Perspective of St Luke", "Religion", 24 , 715, 18, 22.99,3,3 ),
-    ("9780511554773", "And The Two Shall Become One Flesh: A Study of Traditions in Ephesians", "Religion", 27    , 266 , 71.75, 86.99,4,4 ),
-    ("9780511779527", "Armed Attack and Article 51 of the UN Charter: Evolutions in Customary Law and Practice", "Law",  20  , 556, 53, 64.99,5,5 ),
-    ("9780511581816", "By the Banks of the Neva: Chapters from the Lives and Careers of the British in Eighteenth-Century Russia", "European History", 32 , 369 , 29.25, 35.99,6,6 ),
-    ("9781580466806", "By My Absolute Royal Authority: Justice and the Castilian Commonwealth at the Beginning of the First Global Age", "European History",15    , 740 , 66, 79.99,7,7 ),
-    ("9781139872072", "The 'Colored Hero' of Harper's Ferry: John Anthony Copeland and the War against Slavery", "American History", 21   , 523 , 123.75, 149.99,8,8 ),
-    ("9781316271353", "The 'Conspiracy' of Free Trade: The Anglo-American Struggle over Empire and Economic Globalisation", "History Cross Discipline",   22  , 778 , 45.5, 55.99,9,9 ),
-    ("9781846159572", "The 'Continuations' of Chrétien's 'Perceval': Content and Construction, Extension and Ending", "English Literature",  19  , 401, 94.25, 114.99,10,10 );
+    ("9781783085569", "A Midsummer Night's Dream in Context: Magic, Madness and Mayhem", "English Literature", 40 , 336 , 22.5, 27.99, 1, 1 ),
+    ("9780748638482", "The Alids: The First Family of Islam", "Religion", 21  , 613, 20, 24.99, 2, 2 ),
+    ("9780511555121", "And so we Came to Rome: The Political Perspective of St Luke", "Religion", 24 , 715, 18, 22.99, 3, 3 ),
+    ("9780511554773", "And The Two Shall Become One Flesh: A Study of Traditions in Ephesians", "Religion", 27    , 266 , 71.75, 86.99, 4, 4 ),
+    ("9780511779527", "Armed Attack and Article 51 of the UN Charter: Evolutions in Customary Law and Practice", "Law", 20  , 556, 53, 64.99, 5, 5 ),
+    ("9780511581816", "By the Banks of the Neva: Chapters from the Lives and Careers of the British in Eighteenth-Century Russia", "European History", 32 , 369 , 29.25, 35.99, 6, 6 ),
+    ("9781580466806", "By My Absolute Royal Authority: Justice and the Castilian Commonwealth at the Beginning of the First Global Age", "European History", 15    , 740 , 66, 79.99, 7, 7 ),
+    ("9781139872072", "The 'Colored Hero' of Harper's Ferry: John Anthony Copeland and the War against Slavery", "American History", 21   , 523 , 123.75, 149.99, 8, 8 ),
+    ("9781316271353", "The 'Conspiracy' of Free Trade: The Anglo-American Struggle over Empire and Economic Globalisation", "History Cross Discipline", 22  , 778 , 45.5, 55.99, 9, 9 ),
+    ("9781846159572", "The 'Continuations' of Chrétien's 'Perceval': Content and Construction, Extension and Ending", "English Literature", 19  , 401, 94.25, 114.99, 10, 10 );
 
 INSERT INTO Inventories --(BookstoreId, QuantityOnHand, ISBN)
 VALUES
@@ -135,7 +135,7 @@ VALUES
     (5, 4, "9781139872072"),
     (5, 3, "9781316271353"),
     (5, 3, "9781846159572"),
-       --
+    --
     (6, 3, "9781783085569"),
     (6, 3, "9780748638482"),
     (6, 2, "9780511555121"),
@@ -146,7 +146,7 @@ VALUES
     (6, 0, "9781139872072"),
     (6, 1, "9781316271353"),
     (6, 0, "9781846159572"),
-       --
+    --
     (7, 5, "9781783085569"),
     (7, 2, "9780748638482"),
     (7, 0, "9780511555121"),
@@ -157,7 +157,7 @@ VALUES
     (7, 4, "9781139872072"),
     (7, 5, "9781316271353"),
     (7, 5, "9781846159572"),
-       --
+    --
     (8, 4, "9781783085569"),
     (8, 1, "9780748638482"),
     (8, 2, "9780511555121"),
@@ -168,7 +168,7 @@ VALUES
     (8, 1, "9781139872072"),
     (8, 1, "9781316271353"),
     (8, 2, "9781846159572"),
-       --
+    --
     (9, 4, "9781783085569"),
     (9, 0, "9780748638482"),
     (9, 2, "9780511555121"),
@@ -188,9 +188,9 @@ VALUES
     (10, 5, "9780511581816"),
     (10, 0, "9781580466806"),
     (10, 4, "9781139872072"),
-	(10, 2, "9781316271353"),
+    (10, 2, "9781316271353"),
     (10, 1, "9781846159572");
-    
+
 
 INSERT INTO BranchHasBooks --(BranchId, ISBN, Quantity)
 VALUES
