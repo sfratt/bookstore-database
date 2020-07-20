c98f3a8ed4fdba11459f7eeb434de279fefeb8c5
deeppatel-11 <deeppatel770@gmail.com>
2020-07-20 17:54:44 -0400

    query 1 fixed

--- scripts/query_01.sql
+++ scripts/query_01.sql
@@ -1,6 +1,6 @@
 -- i. Get details of all books in stock ordered by year-to-date-qty-sold in descending order.
 
-SELECT ISBN, Author, Title, BookSubject, QuantityOnHand, YearToDateQuantitySold, CostPrice, SellingPrice
+SELECT Books.*
 FROM Books
 WHERE QuantityOnHand > 0
 ORDER BY YearToDateQuantitySold DESC;
